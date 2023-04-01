0ec06fde3ce82e739f4dbcb5285d64719cfcb2f1
Dream4ever <cnhwvista@gmail.com>
2023-04-01 18:03:35 +0800

    build: 调整 tailwind 的配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Press_Start_2P } from 'next/font/google';
+
+const press_Start_2P = Press_Start_2P({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-ps2p',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${press_Start_2P.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{jsx,tsx}', './src/components/**/*.{jsx,tsx},'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ps2p: ['var(--font-ps2p)'],
+      },
+    },
   },
   plugins: [],
 };
